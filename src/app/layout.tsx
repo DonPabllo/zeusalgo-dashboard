d5d733833c11ee64c2384c4222f63384d2a9a3ff
Pablo88 <pablo88frd@gmail.com>
2025-05-15 01:40:57 +0200

    Fix: importat globals.css in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 // src/app/layout.tsx
+import './globals.css'; // <-- Importă stilurile globale
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
